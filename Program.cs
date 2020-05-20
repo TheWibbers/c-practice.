b72bb07680a11d4b0598fe6948b69c23e1c916bd
Sam <samuelthomas342@yahoo.co.uk>
2020-05-20 20:05:30 +0100

    practice for c#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tutorialfollowalong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = ("samuel thomas");
+            int num = 6;
+            num++;
+            // num--; does the opposite
+            // Console.WriteLine(num / 2);
+            // Console.WriteLine(name);
+            // Console.WriteLine(Math.Abs(-40));
+        }
+    }
+}
+   
